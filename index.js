a45b81672975966256aa48d3de2388b9a766b697
Jyoti Kumari <68436635+Jyoti-kumari55@users.noreply.github.com>
2024-09-24 17:58:04 +0530

    vercel deployment

---
+++ index.js
@@ -0,0 +1,82 @@
+require("./db/dbConnect");
+const { Books } = require("./model/bookModel");
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const allbooks = await Books.find();
+    res.json(allbooks);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+app.post("/books", async (req, res) => {
+  const { bookName, author, genre } = req.body;
+
+  try {
+    const bookData = new Books({ bookName, author, genre });
+    await bookData.save();
+    res.status(201).json(bookData);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  const bookId = req.params.id;
+  const updatedBookData = req.body;
+
+  try {
+    const updatedBook = await Books.findByIdAndUpdate(
+      bookId,
+      updatedBookData,
+      { new: true },
+    );
+
+    if (!updatedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json(updatedBook);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+})
+
+app.delete("/books/:id", async (req, res) => {
+  const bookId = req.params.id;
+
+  try {
+    const deletedBook = await Books.findByIdAndDelete(bookId);
+
+    if (!deletedBook) {
+      return res.status(404).json({ error: "Book not found" });
+    }
+
+    res.status(200).json({
+      message: "Book deleted successfully",
+      book: deletedBook,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
